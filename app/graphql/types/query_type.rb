111d0d4912c2c9449269b0bdb3bd562809543d9f
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2018-11-05 21:10:09 -0200

    Added Models and Types Graphql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,5 +9,24 @@ Types::QueryType = GraphQL::ObjectType.define do
     resolve ->(obj, args, ctx) {
       "Hello World!"
     }
+  # chamamos o field de "user" e ele terá um
+  # namespace chamado "Types" que chamamos
+  # de "UserType" e ficará em
   end
+
+  # app/graphql/types/user_type.rb
+    field :user, Types::UserType do
+      # passamos aqui o "id" do User como argumento de consulta
+      # esse valor de "id" vem do front-end para o back-end
+      argument :id, types.ID # o "id" tem um tipo especial chamado ID
+      description "Identificação do Usuário"
+
+      # aqui é o método chamado "resolve" que resgata
+      # os dados de User do banco de dados
+      resolve -> (obj, args, ctx) {
+        User.where(id: args[:id]).first
+        # puts ctx.query.query_string
+      }
+    end
 end
+
